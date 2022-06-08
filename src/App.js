b9919ccb35c8c001ab5322e00c93eddae31252bd
dk001 <kapadiya10719@gmail.com>
2022-06-08 10:52:51 +0530

    updAted commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Header from './Components/layout/Header';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+    //changes  khghfuyfuyf
+    //fuyufjgvjvj
+  );
+}
+
+export default App;
---
+++ src/Components/meals/MealsSummery.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummery.module.css';
+
+const MealsSummery = () => {
+    return (
+        <section className={classes.summery}>
+            <h2>Delicious food, Delivered to you</h2>
+            <p>
+                Choose your favourite meal from our broad selection of available meals 
+                and enjoy a delicious lunch or dinner at home.
+            </p>
+            <p>
+                All our meals are cooked with high-quality ingedients, just-in-time and
+                of course by experienced chefs!
+            </p>
+        </section>
+    )
+}
